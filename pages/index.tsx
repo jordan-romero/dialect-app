33b52a9e335b78c4b9f8365fabbfd1d46fcf34da
jordles113 <jordan.t.romero@gmail.com>
2023-01-07 12:30:09 -0600

    adds about and contact page placeholders

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import PrelaunchHomePage from './PrelaunchHomePage/PrelaunchHomePage'
+import PrelaunchHomePage from '../components/PrelaunchHomePage/PrelaunchHomePage'
 
 export default function Home() {
   return (
