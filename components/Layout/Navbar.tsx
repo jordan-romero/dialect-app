9ef34d004471e69fff762379578d2e6e229a2e12
jordles113 <jordan.t.romero@gmail.com>
2023-05-21 15:56:05 -0500

    updates the menu

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,71 +1,63 @@
-import { ArrowRightIcon, HamburgerIcon } from '@chakra-ui/icons'
-import { Flex, Heading, HStack, IconButton, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverCloseButton, PopoverBody, Menu, MenuItem, MenuList, MenuButton, useDisclosure, Box, Drawer, DrawerOverlay, DrawerContent, DrawerCloseButton, VStack, DrawerBody } from '@chakra-ui/react'
-import React, { useState } from 'react'
-import NavComponent from './NavComponent'
-import useMobileCheck from '../hooks/useMobileCheck'
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Box, Flex, Heading, HStack, IconButton, Menu, MenuItem, MenuButton, MenuList, VStack } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import useMobileCheck from '../hooks/useMobileCheck';
 
 const Navbar = () => {
   const isMobile = useMobileCheck();
 
   if (isMobile) {
-    return (
-      <MobileNavbar />
-    );
+    return <MobileNavbar />;
   }
 
   return (
     <Flex w="100%" h="14" align="center">
       <HStack w="100%" spacing={0} justify="space-between">
         <HStack align="center">
-          <ArrowRightIcon boxSize={8} color="brand.purple" />
           <Heading fontSize="xl" ml={2}>
             Acting Accents
           </Heading>
         </HStack>
-          <HStack spacing="24px" mr={4} pr={8} justifyContent="end">
-            <NavComponent navText="Home" />
-            <NavComponent navText="About" />
-            <NavComponent navText="Testimonials" />
-            <NavComponent navText="Contact" />
-          </HStack>
+        <HStack spacing="24px" mr={4} pr={8} justifyContent="end">
+          <NavComponent navText="Home" />
+          <NavComponent navText="About" />
+          <NavComponent navText="Testimonials" />
+          <NavComponent navText="Contact" />
+        </HStack>
       </HStack>
     </Flex>
   );
 };
 
 const MobileNavbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const [menuItems, setMenuItems] = useState(['home', 'about', 'testimonials', 'contact']);
 
   return (
     <Box>
-      <IconButton
-        aria-label="Toggle menu"
-        icon={<HamburgerIcon />}
-        variant="ghost"
-        onClick={onOpen}
-      />
+      <Menu>
+        <MenuButton
+          as={IconButton}
+          aria-label="Toggle menu"
+          icon={<HamburgerIcon />}
+          variant="ghost"
+        />
 
-      <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerBody>
-            <VStack p="4" spacing="4">
+        <MenuList>
+          <VStack p="4" spacing="4">
             {menuItems.map((item, index) => (
-              <Box key={index} onClick={onClose} as="a" href={`/${item}`}>
+              <MenuItem key={index} onClick={() => window.location.href = `/${item}`} textTransform="capitalize">
                 {item}
-              </Box>
+              </MenuItem>
             ))}
-            </VStack>
-          </DrawerBody>
-        </DrawerContent>
-      </Drawer>
+          </VStack>
+        </MenuList>
+      </Menu>
     </Box>
   );
 };
 
 export default Navbar;
+
 
 
 
