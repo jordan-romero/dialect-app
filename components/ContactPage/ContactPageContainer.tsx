14dddd0eafe32dbd254008e481b5bb386c6f8eee
jordles113 <jordan.t.romero@gmail.com>
2023-06-11 16:26:10 -0500

    contact page functionality

---
+++ components/ContactPage/ContactPageContainer.tsx
@@ -0,0 +1,13 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import ContactPageFormContainer from './ContactPageForm/ContactPageFormContainer'
+
+const ContactPageContainer = () => {
+  return (
+    <Box> 
+        <ContactPageFormContainer />
+    </Box>
+  )
+}
+
+export default ContactPageContainer
---
+++ components/ContactPage/ContactPageForm/ContactPageIcon.tsx
@@ -0,0 +1,13 @@
+import { Box, Icon } from '@chakra-ui/react'
+import React from 'react'
+import { FaEnvelopeOpenText } from 'react-icons/fa'
+
+const ContactPageIcon = () => {
+  return (
+    <Box>
+        <Icon as={FaEnvelopeOpenText} boxSize={96} mr={4} color="brand.purple" />
+    </Box>
+  )
+}
+
+export default ContactPageIcon
