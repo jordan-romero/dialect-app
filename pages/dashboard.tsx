ad772baae01cc2ca5e90c8a567e63a25ba258415
jordles113 <jordan.t.romero@gmail.com>
2023-10-21 16:10:04 -0500

    removes refs to autho

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { withPageAuthRequired } from '@auth0/nextjs-auth0'
 import DashboardPage from '../components/DashboardPage/DashboardPage'
 
 const dashboard = () => {
