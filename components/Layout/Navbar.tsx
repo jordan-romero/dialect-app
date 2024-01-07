393daffbca5acbc2ed555e551b68952984f8eef9
Jordan Romero <jordan.t.romero@gmail.com>
2024-01-07 15:29:24 -0600

    fixes nav error

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -35,47 +35,42 @@ const Navbar = () => {
     return <MobileNavbar />
   }
 
-  return (
-    !isMobile &&
-    !router.pathname.includes('dashboard') && (
-      <Flex w="100%" h="24" align="center" bgColor={'brand.purple'}>
-        <HStack w="100%" justify="space-between">
-          <HStack align="center" bgColor={'brand.purple'} spacing={0}>
-            <Link href="/">
-              <Flex>
-                <Image
-                  src="/actingAccentsLogo.png"
-                  alt="acting accents logo"
-                  objectFit="contain"
-                  h="24"
-                  mr={-6}
-                  w="100%"
-                />
-                <Image
-                  src="/actingAccentsTitle.png"
-                  alt="acting accents title"
-                  objectFit="contain"
-                  h="20"
-                />
-              </Flex>
-            </Link>
-          </HStack>
-          <HStack spacing="24px" mr={4} pr={8} justifyContent="end">
-            <NavComponent navText="Home" />
-            <NavComponent navText="About" />
-            <NavComponent navText="Contact" />
-            {signUpAndLoginVisible && user.user === undefined ? (
-              <Login />
-            ) : null}
-            {signUpAndLoginVisible && user.user !== undefined ? (
-              <NavComponent navText="Dashboard" />
-            ) : null}
-            {signUpAndLoginVisible && user ? <Logout /> : null}
-          </HStack>
+  return !isMobile && !router.pathname.includes('dashboard') ? (
+    <Flex w="100%" h="24" align="center" bgColor={'brand.purple'}>
+      <HStack w="100%" justify="space-between">
+        <HStack align="center" bgColor={'brand.purple'} spacing={0}>
+          <Link href="/">
+            <Flex>
+              <Image
+                src="/actingAccentsLogo.png"
+                alt="acting accents logo"
+                objectFit="contain"
+                h="24"
+                mr={-6}
+                w="100%"
+              />
+              <Image
+                src="/actingAccentsTitle.png"
+                alt="acting accents title"
+                objectFit="contain"
+                h="20"
+              />
+            </Flex>
+          </Link>
         </HStack>
-      </Flex>
-    )
-  )
+        <HStack spacing="24px" mr={4} pr={8} justifyContent="end">
+          <NavComponent navText="Home" />
+          <NavComponent navText="About" />
+          <NavComponent navText="Contact" />
+          {signUpAndLoginVisible && user.user === undefined ? <Login /> : null}
+          {signUpAndLoginVisible && user.user !== undefined ? (
+            <NavComponent navText="Dashboard" />
+          ) : null}
+          {signUpAndLoginVisible && user ? <Logout /> : null}
+        </HStack>
+      </HStack>
+    </Flex>
+  ) : null
 }
 
 const MobileNavbar = () => {
